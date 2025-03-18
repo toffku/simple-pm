7884e58a1e0e1875bf3c08b30fe7245132c41fe4
toffku <kristoff.urcia.2021@uni.strath.ac.uk>
2025-03-18 16:59:32 +0000

    Sidebar and ThemeProvider implemented

--- client/simple-pm/src/App.tsx
+++ client/simple-pm/src/App.tsx
@@ -1,5 +1,24 @@
+import AppSidebar from "./components/AppSidebar";
+import { ModeToggle } from "./components/ModeToggle";
+import { ThemeProvider } from "./components/ThemeProvider";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            <ModeToggle />
+            <Dashboard />
+          </main>
+        </SidebarProvider>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ client/simple-pm/src/components/AppSidebar.tsx
@@ -0,0 +1,82 @@
+import {
+  Sidebar,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import {
+  Calendar,
+  Home,
+  Inbox,
+  LucideProps,
+  Search,
+  Settings,
+} from "lucide-react";
+
+interface SidebarTypes {
+  title: string;
+  url: string;
+  icon: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+}
+
+const items: Array<SidebarTypes> = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+const AppSidebar = () => {
+  return (
+    <>
+      <Sidebar>
+        {/* <SidebarContent /> */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </Sidebar>
+    </>
+  );
+};
+
+export default AppSidebar;
